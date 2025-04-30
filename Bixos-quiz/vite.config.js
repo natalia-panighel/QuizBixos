ce61f93f30f02db27888c4e38679166c8e682d8d
Natália Penalva Panighel <40546071+natalia-panighel@users.noreply.github.com>
2025-04-30 20:53:41 -0300

    Update vite.config.js

--- Bixos-quiz/vite.config.js
+++ Bixos-quiz/vite.config.js
@@ -9,5 +9,7 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     allowedHosts: ['4207a8d8-3a95-490d-8688-b28796f461db-00-1x595ctwifvkz.riker.replit.dev', 'all']
+  base: "./", // <- necessário para builds relativas
+  plugins: [react()],
   }
-})
+});
