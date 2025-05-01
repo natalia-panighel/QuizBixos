c52891019e51079e32da772e7e5b34d9faf9e5c3
Natália Penalva Panighel <40546071+natalia-panighel@users.noreply.github.com>
2025-04-30 21:03:53 -0300

    Update vite.config.js

--- Bixos-quiz/vite.config.js
+++ Bixos-quiz/vite.config.js
@@ -9,7 +9,13 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     allowedHosts: ['4207a8d8-3a95-490d-8688-b28796f461db-00-1x595ctwifvkz.riker.replit.dev', 'all']
+  }
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
   base: "./", // <- necessário para builds relativas
   plugins: [react()],
-  }
+})
 });
